2e5b466e88389daab374041fc4f92573f81f0dfd
Colby Fayock <fayock@gmail.com>
2020-03-11 09:47:16 -0400

    yarn, removing xcode - more headaches than worth with specific snippet

--- configure.sh
+++ configure.sh
@@ -10,17 +10,6 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true; # show all file
 killall Dock 2>/dev/null;
 killall Finder 2>/dev/null;
 
-# install Xcode Command Line Tools
-# https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-PROD=${PROD//"Label: "/}
-softwareupdate -i "$PROD" --verbose;
-
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "ZSH_DISABLE_COMPFIX=true\n\n$(cat ~/.zshrc)" > ~/.zshrc # https://github.com/ohmyzsh/ohmyzsh/issues/6835
@@ -37,6 +26,9 @@ mkdir ~/.nvm
 . $(brew --prefix nvm)/nvm.sh
 nvm install 12
 nvm alias default 12
+
+# Install Yarn
+brew install yarn
 
 # Add nvm autoload when navigatin to folder
 echo "$(cat ./nvm/source.txt)\n\n$(cat ~/.zshrc)" > ~/.zshrc
